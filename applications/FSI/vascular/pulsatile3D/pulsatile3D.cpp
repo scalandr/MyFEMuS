c0220b977bed333a2eaddbeab34e5b98bf255575
scalandr <scalandrini@fsu.edu>
2019-09-10 22:27:25 -0600

    quanah: pulsatile3D with 1 refinement for 5 s

--- applications/FSI/vascular/pulsatile3D/pulsatile3D.cpp
+++ applications/FSI/vascular/pulsatile3D/pulsatile3D.cpp
@@ -171,7 +171,7 @@ int main ( int argc, char ** args )
   // ******* Init multilevel mesh from mesh.neu file *******
   unsigned short numberOfUniformRefinedMeshes, numberOfAMRLevels;
 
-  numberOfUniformRefinedMeshes = 2;
+  numberOfUniformRefinedMeshes = 1;
   numberOfAMRLevels = 0;
 
   std::cout << 0 << std::endl;
@@ -434,7 +434,7 @@ int main ( int argc, char ** args )
   if ( iproc == 0 ) {
     std::ofstream outf;
     if ( simulation == 0 ) {
-      outf.open ( "DataPrint_Turek_3D_2refLevels.txt" );
+      outf.open ( "DataPrint_Turek_3D_1refLevels.txt" );
     }
     else if ( simulation == 2 ) {
       outf.open ( "DataPrint_aorta.txt" );
